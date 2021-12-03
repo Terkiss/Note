0679d0bbe33333fd304e909ea5a7c0c67a79f06b
Terkiss <dl2000dl@naver.com>
2021-12-04 00:51:58 +0900

    20211204

--- Unity/Epic Project/Database/Select.sql
+++ Unity/Epic Project/Database/Select.sql
@@ -16,3 +16,10 @@ select a.skillNumID, a.skillType, a.attackProperty, a.damage, a.subDamage, a.ski
 SELECT chrINfo.chrID, chrINfo.skillNumID, detail.name, detail.description, s.skillType, s.attackProperty, s.damage, s.subDamage, 
 s.skillRunDuration, s.soundFX, s.attackFXSlot1, s.attackFXSlot2, s.attackFXSlot3, s.attackFXSlot4 FROM character_skill_info chrINfo 
 INNER JOIN skilldetailinfotbl detail ON chrINfo.skillNumID = detail.skillNumID INNER JOIN skills s ON detail.skillNumID = s.skillNumID;
+
+
+
+// 특정 캐릭터 스킬 조회 sql문
+SELECT detail.name, detail.description, s.skillType, s.attackProperty, s.damage, s.subDamage, 
+s.skillRunDuration, s.soundFX, s.attackFXSlot1, s.attackFXSlot2, s.attackFXSlot3, s.attackFXSlot4 FROM character_skill_info chrINfo 
+INNER JOIN skilldetailinfotbl detail ON chrINfo.skillNumID = detail.skillNumID INNER JOIN skills s ON detail.skillNumID = s.skillNumID where chrINfo.chrid = "";
